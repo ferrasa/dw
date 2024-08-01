0e0989593e1506dacd924d3606a9ac4881bdf1a0
ferrasa <ferrasa@gmail.com>
2024-08-01 09:54:15 -0300

    DW 2024

--- agenda/server.js
+++ agenda/server.js
@@ -1,5 +1,7 @@
 import agenda from './db.js';
 import express from 'express';
+import { contatoModel, contatoModelUpdate } from './check.js';
+
 
 const app = express();
 
@@ -12,6 +14,10 @@ app.get('/', (req, res) => {
 app.get('/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const contato = agenda.find( (c) => c.id === id);
+    if (!contato) {
+        res.status(404).send({mens: 'Não existe um contato para o ID informado.'});
+        return; // sai da execução do método
+    }
     res.status(200).send(contato);
 });
 
@@ -19,7 +25,19 @@ app.put('/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const contato = agenda.find( (c) => c.id === id);
 
+    if (!contato) {
+        res.status(404).send({mens: 'Não existe um contato para o ID informado.'});
+        return; // sai da execução do método
+    }
+
     const contatoAlterar = req.body;
+
+    const {error} = contatoModelUpdate.validate(contatoAlterar);
+    if (error){
+        //error.details[0].message : obtem somente o texto da mensagem de erro.
+        res.status(400).send({mens: error.details[0].message});
+        return; // sai da execução do método
+    }
 
     const campos = Object.keys(contatoAlterar);
     console.log(campos);
@@ -32,14 +50,26 @@ app.put('/:id', (req, res) => {
 app.post('/', (req, res) => {
     const contato = req.body;
 
+    const {error} = contatoModel.validate(contato);
+    if (error){
+        //error.details[0].message : obtem somente o texto da mensagem de erro.
+        res.status(400).send({mens: error.details[0].message});
+        return; // sai da execução do método
+    }
+
     agenda.push(contato);
     res.status(201).send(contato);
 });
 
 app.delete('/:id', (req, res) => {
-    const id = req.params.id;
+    const id = parseInt(req.params.id);
     const indiceContato = agenda.findIndex((c) => c.id === id);
 
+    if (indiceContato === -1) {
+        res.status(404).send({mens: 'Não existe um contato para o ID informado.'});
+        return; // sai da execução do método
+    }
+    
     const contatoRemovido = agenda.splice(indiceContato, 1);
     res.status(200).send(contatoRemovido);
 });
