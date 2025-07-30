de1c7c038f8ef12eeba805e29ad8be4260d97444
ferrasa <ferrasa@gmail.com>
2025-07-30 18:57:00 -0300

    dw

---
+++ agenda/db.js
@@ -0,0 +1,20 @@
+const agenda = [
+    {
+        id : 1,
+        nome : 'Adamastor da Silva',
+        telefone : '4299999999',
+        email : 'ada@gmail.com',
+        nota : 'Gosta de jogar peteca',
+        ativo : true
+    },
+    {
+        id : 2,
+        nome : 'Gervasio da Silva',
+        telefone : '4299991111',
+        email : 'gerva@gmail.com',
+        nota : 'Gosta de jogar truco',
+        ativo : true
+    }
+];
+
+export default agenda;
