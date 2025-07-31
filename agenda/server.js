cc162f46c9d9f9a93d24d3a9cea1c128c7d8706e
ferrasa <ferrasa@gmail.com>
2025-07-31 11:01:13 -0300

    dw

--- agenda/server.js
+++ agenda/server.js
@@ -1,1 +1,22 @@
 import agenda from "./db.js";
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.status(200).send(agenda);
+}); 
+
+app.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const contato = agenda.find( (c) => c.id === id );
+
+    if (!contato){
+        res.status(404).send({mens : 'Não existe ID'});
+        return; // sai da execução do método
+    }
+    res.status(200).send(contato);
+})
+
+app.listen(3000);
+console.log('AGENDA - API WEB executando');
