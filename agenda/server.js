e32563bf6877dad2036577d839225fdbbbce7b54
ferrasa <ferrasa@gmail.com>
2025-08-06 19:26:08 -0300

    dw

--- agenda/server.js
+++ agenda/server.js
@@ -2,6 +2,7 @@ import agenda from "./db.js";
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) =>{
     res.status(200).send(agenda);
@@ -16,7 +17,37 @@ app.get('/:id', (req, res) => {
         return; // sai da execução do método
     }
     res.status(200).send(contato);
-})
+});
+
+app.post('/', (req, res) => {
+    const contato = req.body;
+
+    agenda.push(contato);
+    res.status(201).send(contato);
+});
+
+app.delete('/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    const indiceContato = agenda.findIndex( (c) => c.id === id);
+    agenda.splice(indiceContato, 1);
+    
+    res.status(200).send(agenda);
+
+});
+
+app.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const contato = agenda.find( (c) => c.id === id);
+
+    const contatoAlterar = req.body;
+    const atributos = Object.keys(contatoAlterar);
+    
+    for (let c of atributos)
+        contato[c] = contatoAlterar[c];
+
+    res.status(201).send(contato);
+
+});
 
 app.listen(3000);
 console.log('AGENDA - API WEB executando');
