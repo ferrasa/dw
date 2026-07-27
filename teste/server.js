0015fd66e06fb58765de27e3d846dabc5fc1c54c
ferrasa <ferrasa@gmail.com>
2026-07-27 17:03:04 -0300

    dw

---
+++ teste/server.js
@@ -0,0 +1,12 @@
+import http from 'http';
+
+function processa(req, res){
+    if (req.method === 'GET' && req.url === '/teste'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h1>Funcionou</h1>');
+        res.end();
+    }
+}
+const servidor = http.createServer(processa);
+servidor.listen(3000);
+console.log('Servidor ON');
